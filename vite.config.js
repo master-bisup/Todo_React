530241018c0bad22f3b2646f79318506325647a6
Biswombhara <biswombhara.patra@gmail.com>
2024-03-23 12:08:44 +0530

    Modified packages for deployment-2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+//https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Todo_React/',
   plugins: [react()],
 })
